4ac122526c165f02a683b7fff2f58e8900440f2a
王跃博 <123456>
2019-08-11 01:32:06 +0800

    整合themlef

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,7 +1,11 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @program: demo
@@ -9,12 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
  * @author: wangyuebo
  * @create: 2019-07-25 18:09
  */
-@RestController
+@Controller
 public class HelloController {
 
     @RequestMapping("/hello")
-    public String index(){
-        return "hello,world";
+    public String index(HttpServletRequest request, @RequestParam(value = "name", required = false, defaultValue = "springboot-thymeleaf")
+            String name){
+        request.setAttribute("name", name);
+        return "hello";
     }
 
 
